54f22a5d12b390a4bd76ec7a19f05010d63ff23a
Martin Evald <martin.evald@gmail.com>
2019-12-19 12:54:49 +0100

    Refactored substitution code a bit (less code inside the switch statement).

--- src/substitute.c
+++ src/substitute.c
@@ -6,7 +6,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-static mtpl_result mtpl_perform_substitution(
+static mtpl_result lookup_generator(
+    const mtpl_hashtable* generators,
+    mtpl_readbuffer* source,
+    mtpl_generator* out_generator
+) {
+    char gen_name[MTPL_GENERATOR_NAME_MAXLEN + 1] = { 0 };
+    uint8_t gen_read_chars = 0;
+    while (
+        gen_read_chars <= MTPL_GENERATOR_NAME_MAXLEN 
+            && source->data[++(source->cursor)] != '>'
+    ) {
+        gen_name[gen_read_chars++] = source->data[source->cursor];
+    }
+
+    if (source->data[source->cursor] != '>') {
+        return MTPL_ERR_MALFORMED_NAME;
+    }
+    source->cursor++;
+    gen_name[gen_read_chars] = '\0';
+    *out_generator = *(void**) mtpl_htable_search(gen_name, generators);
+    if (!out_generator) {
+        return MTPL_ERR_UNKNOWN_KEY;
+    }
+    return MTPL_SUCCESS;
+}
+
+static mtpl_result perform_substitution(
     mtpl_generator generator,
     const mtpl_allocators* allocators,
     mtpl_readbuffer* source,
@@ -19,35 +45,17 @@ static mtpl_result mtpl_perform_substitution(
         .data = allocators->malloc(MTPL_DEFAULT_BUFSIZE),
         .size = MTPL_DEFAULT_BUFSIZE
     };
-    char gen_name[MTPL_GENERATOR_NAME_MAXLEN + 1] = { 0 };
-    uint8_t gen_read_chars = 0;
     mtpl_generator sub_generator;
 
     while (true) {
         switch (source->data[source->cursor]) {
         case '[':
             // Lookup generator name and begin new substitution.
-            while (
-                gen_read_chars <= MTPL_GENERATOR_NAME_MAXLEN 
-                    && source->data[++(source->cursor)] != '>'
-            ) {
-                gen_name[gen_read_chars++] = source->data[source->cursor];
-            }
-
-            if (source->data[source->cursor] != '>') {
-                result = MTPL_ERR_MALFORMED_NAME;
+            result = lookup_generator(generators, source, &sub_generator);
+            if (result != MTPL_SUCCESS) {
                 goto finish_substitution;
             }
-            source->cursor++;
-            gen_name[gen_read_chars] = '\0';
-            sub_generator = *(void**) mtpl_htable_search(gen_name, generators);
-            memset(gen_name, 0, sizeof(gen_name));
-            gen_read_chars = 0;
-            if (!sub_generator) {
-                result = MTPL_ERR_UNKNOWN_KEY;
-                goto finish_substitution;
-            }
-            result = mtpl_perform_substitution(
+            result = perform_substitution(
                 sub_generator,
                 allocators,
                 source,
@@ -106,7 +114,7 @@ mtpl_result mtpl_substitute(
         .cursor = 0,
         .size = 0
     };
-    return mtpl_perform_substitution(
+    return perform_substitution(
         mtpl_generator_copy,
         allocators,
         &buffer,
