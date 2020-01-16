b1c0a2be0ca562d670ba6d660acb3a15f3ea9d39
Martin Evald <martin.evald@gmail.com>
2020-01-16 19:23:27 +0100

    Bumped release version.

--- include/mintpl/version.h
+++ include/mintpl/version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define MINTPL_VERSION_MAJOR 1
-#define MINTPL_VERSION_MINOR 5
+#define MINTPL_VERSION_MAJOR 2
+#define MINTPL_VERSION_MINOR 0
 #define MINTPL_VERSION_PATCH 0
 
 const char* mtpl_version();
