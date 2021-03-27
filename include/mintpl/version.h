4f44b461e8716416d3e7adb81424398ec88ff44d
Martin Evald <martin.evald@gmail.com>
2021-03-27 19:54:39 +0100

    Version 3.0.0

--- include/mintpl/version.h
+++ include/mintpl/version.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define MINTPL_VERSION_MAJOR 2
-#define MINTPL_VERSION_MINOR 1
-#define MINTPL_VERSION_PATCH 2
+#define MINTPL_VERSION_MAJOR 3
+#define MINTPL_VERSION_MINOR 0
+#define MINTPL_VERSION_PATCH 0
 
 const char* mtpl_version();
 
